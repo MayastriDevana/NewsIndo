c9067b5bfb416771900e32be3f5b3095591a97fc
Maya <Mayastridevana0205@gmail.com>
2024-04-23 08:47:30 +0700

    Initial Commit

---
+++ NewsIndo/NewsIndoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsIndoApp.swift
+//  NewsIndo
+//
+//  Created by User on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsIndoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
