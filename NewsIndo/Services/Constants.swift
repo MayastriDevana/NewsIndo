9a9ab2b7e7a7c85f670c2994e3b85d404b3c5807
Maya <Mayastridevana0205@gmail.com>
2024-04-23 09:54:15 +0700

    API Service Fetch News

---
+++ NewsIndo/Services/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  NewsIndo
+//
+//  Created by User on 23/04/24.
+//
+
+import Foundation
+
+struct Constants {
+    static let newsUrl =
+    "https://berita-indo-api-next.vercel.app/api/kumparan-news/"
+    
+    static let newsTitle = "Berita Indo"
+}
